16af889d61f64f4bd9600cd635b8b99880856ef2
tudbut <7401dbfa221176f0601fc90c1ada24467e4193d2>
2021-05-05 16:06:37 +0200

    new message payload
    rats shall die

--- src/main/groovy/tudbut/WebHookRatKiller.groovy
+++ src/main/groovy/tudbut/WebHookRatKiller.groovy
@@ -4,6 +4,10 @@ import tudbut.net.http.HTTPContentType
 import tudbut.net.http.HTTPHeader
 import tudbut.net.http.HTTPRequest
 import tudbut.net.http.HTTPRequestType
+import tudbut.parsing.JSON
+import tudbut.parsing.TCN
+import tudbut.parsing.TCNArray
+import tudbut.tools.Lock
 
 class WebHookRatKiller {
 
@@ -14,23 +18,59 @@ class WebHookRatKiller {
         }
         String hook = args[0]
 
+        TCN ratShat = new TCN()
+        TCNArray embeds = new TCNArray()
+        TCN embed = new TCN()
+        TCNArray embedFields = new TCNArray()
+        TCN field = new TCN()
+        for (i in 0..<10) {
+            embeds.add(embed)
+        }
+        embed.set("fields", embedFields)
+        for (i in 0..<25) {
+            embedFields.add(field)
+        }
+        ratShat.set("tts", true) // Yum
+        ratShat.set("content", "@everyone your rat got shat on by TudbuT EZZZZ")
+        ratShat.set("username", "TudbuT#2624")
+        ratShat.set("embeds", embeds)
+        embed.set("title", "TudbuT\non top")
+        embed.set("description", "ez\n" * 3)
+        field.set("name", "TudbuT\non top")
+        field.set("value", "ez\n" * 3)
+
         URL parsed = new URL(hook)
 
+        Lock lock = new Lock()
+        long rateLimit
         HTTPRequest request = new HTTPRequest(
                 HTTPRequestType.POST, "https://" + parsed.getHost(), 443, parsed.getPath(), HTTPContentType.JSON,
-                '{"content":"@everyone your rat got shat on by TudbuT EZZZZ","username":"TudbuT#2624"}',
+                JSON.write(ratShat),
                 new HTTPHeader("User-Agent", "Java")
         )
-        for (int i = 0 ; i < 60 ; i++) {
-            println request.send().parse().getBody()
+        for (int i = 1 ; i <= 60 ; i++) {
+            def s = request.send().parse().getBody()
+            if (s != '') {
+                Long l =JSON.read(s).getLong("retry_after")
+                if(l != null) {
+                    rateLimit = l
+                }
+                else {
+                    println 'This webhook got killed already.'
+                    return
+                }
+            } else
+                rateLimit = 0
+            lock.lock(rateLimit + 5000 as int)
+            println "Sent $i"
         }
 
-        Thread.sleep(60000)
+        lock.waitHere()
         def s = ""
         for (int i = 0; i < 40 && s != '{"message": "Unknown Webhook", "code": 10015}' ; i++) {
             request = new HTTPRequest(
                     HTTPRequestType.POST, "https://" + parsed.getHost(), 443, parsed.getPath(), HTTPContentType.JSON,
-                    '{"content": "@everyone Say goodbye to your webhook :)","username":"TudbuT#2624"}',
+                    '{"content": "@everyone Say goodbye to your webhook (=\\nTudbuT#2624 on top!","username":"TudbuT#2624"}',
                     new HTTPHeader("User-Agent", "Java")
             )
             HTTPRequest kill = new HTTPRequest(
@@ -38,9 +78,10 @@ class WebHookRatKiller {
                     "{}",
                     new HTTPHeader("User-Agent", "Java")
             )
-            println request.send().parse().getBody()
+            request.send()
             Thread.sleep(2000)
-            println "Kill request: ${s = kill.send().parse().getBody()}"
+            s = kill.send().parse().getBody()
+            println 'Kill request sent.'
         }
 
         println 'Killed.'
